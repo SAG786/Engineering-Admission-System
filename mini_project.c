1d17e6283d760e11b83155db3ed2b82696ae6edd
SAG786 <45678893+SAG786@users.noreply.github.com>
2018-12-07 09:56:10 +0530

    Add files via upload

---
+++ mini_project.c
@@ -0,0 +1,379 @@
+/*Roll no -2016BTECS00036
+NAME - Abhishek Ravindra Sapate
+Roll no- 2016BTEC00046
+NAME - Sourabh Anil Ghorpade
+Project Name-Student's Admission To Computer Science Course Degree */
+#include"mini_project.h"
+void student();
+void faculty();
+int main()
+{
+	int opt,no;
+	char c[7];
+	init(&p);
+	NODE *b;
+	FILE *f=fopen("stu.txt","r");
+	if(!f)
+	{
+		printf("\nEmpty File");
+	}
+	if(f)
+	{
+		fscanf(f,"%d %d %d %d %d %d %d %d %d %d %d",&ct1s1,&ct1s2,&ct2s1,&ct2s2,&cl1s1,&cl1s2,&cl2s1,&cl2s2,&cel1,&cel2,&cel3);
+		while(1)
+		{
+			NODE *a=(NODE *)malloc(sizeof(NODE));
+		   	fscanf(f,"%s %d %s %d %d %d",c,&a->rno,a->nm,&a->d,&a->m,&a->y);
+			a->next=a->eln=a->t1n=a->t2n=a->l1n=a->l2n=NULL;
+			if(feof(f))
+				break;
+			if(strcmp(c,"Registered")==0)
+			{
+				b=p.st;
+				if(p.st==NULL)
+					p.st=a;
+				else
+				{
+					while(b->next)
+						b=b->next;
+					b->next=a;
+				}
+			}
+			else
+			{
+				b=p.st;
+				while(b->rno!=a->rno)
+					b=b->next;
+				a=b;
+				if(strcmp(c,"3SE201")==0)
+				{
+					b=p.t1s1;
+					if(b==NULL)
+						p.t1s1=a;
+					else
+					{
+						while(b->t1n)
+							b=b->t1n;
+						b->t1n=a;
+					}
+				}
+				else if(strcmp(c,"3DM201")==0)
+				{
+					b=p.t1s2;
+					if(b==NULL)
+						p.t1s2=a;
+					else
+					{
+						while(b->t1n)
+							b=b->t1n;
+						b->t1n=a;
+					}
+				}
+				else if(strcmp(c,"3DS201")==0)
+				{
+					b=p.t2s1;
+					if(b==NULL)
+						p.t2s1=a;
+					else
+					{
+						while(b->t2n)
+							b=b->t2n;
+						b->t2n=a;
+					}
+				}
+				else if(strcmp(c,"3OS201")==0)
+				{
+					b=p.t2s2;
+					if(b==NULL)
+						p.t2s2=a;
+					else
+					{
+						while(b->t2n)
+							b=b->t2n;
+						b->t2n=a;
+					}
+				}
+				else if(strcmp(c,"3PA252")==0)
+				{
+					b=p.l1s1;
+					if(b==NULL)
+						p.l1s1=a;
+					else
+					{
+						while(b->l1n)
+							b=b->l1n;
+						b->l1n=a;
+					}
+				}
+				else if(strcmp(c,"3DB252")==0)
+				{
+					b=p.l1s2;
+					if(b==NULL)
+						p.l1s2=a;
+					else
+					{
+						while(b->l1n)
+							b=b->l1n;
+						b->l1n=a;
+					}
+				}
+				else if(strcmp(c,"3CN252")==0)
+				{
+					b=p.l2s1;
+					if(b==NULL)
+						p.l2s1=a;
+					else
+					{
+						while(b->l2n)
+							b=b->l2n;
+						b->l2n=a;
+					}
+				}
+				else if(strcmp(c,"3PL252")==0)
+				{
+					b=p.l2s2;
+					if(b==NULL)
+						p.l2s2=a;
+					else
+					{
+						while(b->l2n)
+							b=b->l2n;
+						b->l2n=a;
+					}
+				}
+				else if(strcmp(c,"3IM201")==0)
+				{
+					b=p.els1;
+					if(b==NULL)
+						p.els1=a;
+					else
+					{
+						while(b->eln)
+							b=b->eln;
+						b->eln=a;
+					}
+				}
+				else if(strcmp(c,"3AI201")==0)
+				{
+					b=p.els2;
+					if(b==NULL)
+						p.els2=a;
+					else
+					{
+						while(b->eln)
+							b=b->eln;
+						b->eln=a;
+					}
+				}
+				else
+				{
+					b=p.els3;
+					if(b==NULL)
+						p.els3=a;
+					else
+					{
+						while(b->eln)
+							b=b->eln;
+						b->eln=a;
+					}
+				}
+		}
+	}
+	}
+	printf("\n*****Computer Science Degree Course Admission*****\n");
+	b=p.st;
+	if(p.st!=NULL)
+	{
+		
+		while(b->next)
+		{
+			b=b->next;
+		}
+		printf("Last Modified Date:\t%d/%d/%d\n",b->d,b->m,b->y);
+	}
+	else
+		d=m=y=0;
+	printf("\nEnter today's date :\t");
+	do{
+		scanf("%d/%d/%d",&d,&m,&y);
+		if(!isvalid(d,m,y))
+		{	
+			printf("Enter Valid Date:");
+			continue;
+		}
+		if(b==NULL)
+			break;
+		if((d>=b->d && m==b->m && y==b->y) || (m>b->m && y==b->y) || y>b->y)
+			break;
+		else
+			printf("Enter Valid Date :\n");
+	}while(1);
+	  while(1)
+		{
+				printf("\nDepartment Of computer science & engineering ");	
+				printf("\n****MENU****\n1.Student\n2.Faculty\n3.Exit\nYour Option:\t");
+				scanf("%d",&opt);
+				if(opt<0)
+				{
+					printf("\nInvalid Input");
+					continue;
+				}
+				if(opt>2)
+				{
+					break;
+				}				
+				switch(opt)
+				{
+					case 1:
+						student();
+						break;
+					case 2:
+						faculty();
+				}
+		}
+			return 0;
+}
+
+void student()
+{
+	int opt,no;
+	while(1)
+	{
+	  printf("\n****MENU****\n1.Register\n2.Display All\n3.Change in Elective Course\n4.Exit\nYour Option:\t");
+	  scanf("%d",&opt);
+		if(opt<0)
+		{
+			printf("\nInvalid Input");
+			continue;
+		}
+	  if(opt>3 )
+	    {
+		save(&p);
+		break;
+	    }
+	  switch(opt)
+	  {
+		case 1:
+			insert(&p);
+			break;
+		case 2:
+			display(&p);
+			break;
+		case 3:
+			printf("Enter Roll No : \t");
+			scanf("%d",&no);
+	    		NODE *b=ispresent(&p,no);
+			if(b)
+			{
+				if((d-(b->d)<7 && m-b->m==0 && y-b->y==0) || (m-(b->m)<=1 && (b->d)-d>23 && y-b->y==0) || (y-(b->y)==1 && (b->m)-m==11 && (b->d)-d>23))
+					change(&p,no);
+				else
+					printf("\nSorry you are unable to change");
+			}
+			else
+				printf("\nEntered Roll No is Unregistered: \t");
+		
+	 }
+	}
+}
+void faculty()
+{
+		int opt;
+		while(1)
+		   {
+			printf("\n****MENU****\n1.Display All\n2.Display theory 1\n3.Display theory 2\n4.Display Lab 1\n5.Display Lab 2\n6.Display Elective\n7.Exit\nYour Option :\t");
+			scanf("%d",&opt);
+			if(opt<0)
+			{
+				printf("\nInvalid Input");
+				continue;
+			}
+			if(opt>6)
+			{
+				save(&p);
+				printf("\n****Count For CSE Dept.****\n%-30s%-15s\n%30s","Subjects","Count","\n   Theory");
+				printf("\n1.%-30s%-15d\n2.%-30s%-15d","Software Engg.",ct1s1,"Discrete Mathematics",ct1s2);
+				printf("\n1.%-30s%-15d\n2.%-30s%-15d\n%30s","Data Structure",ct2s1,"Operating Systems",ct2s2,"\n   Lab");
+				printf("\n1.%-30s%-15d\n2.%-30s%-15d","Processor Architecture",cl1s1,"Data Base",cl1s2);
+				printf("\n1.%-30s%-15d\n2.%-30s%-15d\n%30s","Computer Networks",cl2s1,"Programming Lab 1",cl2s2,"\n   Elective");				
+				printf("\n1.%-30s%-15d\n2.%-30s%-15d\n3.%-30s%-15d","Image Processing",cel1,"Artificial Intelligence",cel2,"Machine Learning",cel3);
+				return;
+			}
+			switch(opt)
+			{
+			
+			case 1:
+				display(&p);
+				break;
+			case 2:
+				printf("\nChoose\n1.3SE201\t\t2.3DM201\nYour option:\t");
+				i:	scanf("%d",&opt);
+				if(opt>2)
+				{
+				printf("\nEnter Option either 1 or 2:\t");
+		        goto i;
+				}
+				if(opt==1)
+					displayt1(&p,p.t1s1);
+				else
+					displayt1(&p,p.t1s2);
+				break;	
+			case 3:
+				printf("\nChoose\n1.3DS201\t\t2.3OS201\nYour option:\t");
+				k:	scanf("%d",&opt);
+				if(opt>2)
+				{
+				printf("\nEnter Option either 1 or 2:\t");
+		        goto k;
+		    	}
+				if(opt==1)
+					displayt2(&p,p.t2s1);
+				else
+					displayt2(&p,p.t2s2);			
+				break;
+			case 4:
+				printf("\nChoose\n1.3PA252\t\t2.3DB252\nYour option:\t");
+				l:	scanf("%d",&opt);
+				if(opt>2)
+				{
+				printf("\nEnter Option either 1 or 2:\t");
+		        goto l;
+		    	}
+				if(opt==1)
+					displayl1(&p,p.l1s1);
+				else
+					displayl1(&p,p.l1s2);
+				break;
+			case 5:
+				printf("\nChoose\n1.3CN252\t\t2.3PL252\nYour option:\t");
+				m:	scanf("%d",&opt);
+				if(opt>2)
+				{
+				printf("\nEnter Option either 1 or 2:\t");
+		        goto m;
+		    	}
+				if(opt==1)
+					displayl2(&p,p.l2s1);
+				else
+					displayl2(&p,p.l2s2);
+				break;
+			case 6:
+				printf("\nChoose\n1.3IM201\t\t2.3AI201\t\t3.ML201\nYour option:\t");
+				n:	scanf("%d",&opt);
+				if(opt>3)
+				{
+				printf("\nEnter Option either 1 or 2 or 3:\t");
+		        goto n;
+		    	}	
+				if(opt==1)
+					displayel(&p,p.els1);
+				else if(opt==2)	
+					displayel(&p,p.els2);	
+				else
+					displayel(&p,p.els3);
+				break;		
+				
+			}
+	    }	
+}
+
---
+++ mini_project.h
@@ -0,0 +1,680 @@
+/*Roll no -2016BTECS00036
+NAME - Abhishek Ravindra Sapate
+Roll no- 2016BTEC00046
+NAME - Sourabh Anil Ghorpade
+Project Name-Student's Admission To Computer Science Course Degree */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+typedef struct node
+{
+	int rno;
+	char nm[20];
+	int d,m,y;
+	struct node *next,*t1n,*t2n,*l1n,*l2n,*eln;	
+}NODE;
+
+typedef struct list
+{
+	NODE *st,*t1s1,*t1s2,*t2s1,*t2s2,*l1s1,*l1s2,*l2s1,*l2s2,*els1,*els2,*els3;
+}LIST;
+
+void init(LIST *t)							//Initializing  all pointers to NULL
+{
+	 t->st=NULL,t->t1s1=NULL,t->t1s2=NULL,t->t2s1=NULL,t->t2s2=NULL,t->l1s1=NULL,t->l1s2=NULL,t->l2s1=NULL,t->l2s2=NULL,t->els1=NULL,t->els2=NULL,t->els3=NULL;
+}
+int d,m,y;
+int ct1s1=0,ct1s2=0,ct2s1=0,ct2s2=0,cl1s1=0,cl1s2=0,cl2s1=0,cl2s2=0,cel1=0,cel2=0,cel3=0;
+
+NODE *ispresent(LIST *t,int i)			    //Checking entered roll no node is present in list
+{
+	NODE *z=t->st;
+	while(z)
+	{
+		if(z->rno==i)
+			break;
+		z=z->next;
+	}
+	return z;
+}
+
+void insert(LIST *t)						//Inserting a node in the list 
+{
+	int opt=0,no;
+	NODE *a=(NODE *)malloc(sizeof(NODE));
+	printf("\nEnter Details:");
+	printf("\nRoll no.:\t");
+	while(1)
+	{
+		if(opt)
+			printf("Roll No. is already Registered...\nEnter New Roll No.:\t");
+		scanf("%d",&a->rno);
+		if(ispresent(t,a->rno))
+			opt=1;
+		else
+			break;
+	}
+	printf("Name:\t\t");
+	scanf("%s",a->nm);
+	printf("\n%-40s\n%-30s%-15s\n1.%-30s1) %-15s\n2.%-30s2) %-15s\n","Options for theory 1 subject","Subject name"," Course code","Software Engg.","3SE201","Discrete Mathematics","3DM201");
+	printf("\nEnter Choice for theory 1:\t");
+a:	scanf("%d",&opt);
+	if(opt==1)
+	{
+		NODE *b=t->t1s1;
+		if(b==NULL)
+			t->t1s1=a;
+		else
+		{
+			while(b->t1n)
+				b=b->t1n;
+			b->t1n=a;
+		}
+		ct1s1++;
+	}
+	else if(opt==2)
+	{
+		NODE *b=t->t1s2;
+		if(b==NULL)
+			t->t1s2=a;
+		else
+		{
+			while(b->t1n)
+				b=b->t1n;
+			b->t1n=a;
+		}
+		ct1s2++;
+	}
+	else
+	{
+		printf("\nEnter Option either 1 or 2:\t");
+		goto a;
+	}
+	printf("\n%-40s\n%-30s%-15s\n1.%-30s1) %-15s\n2.%-30s2) %-15s\n","Options for theory 2 subject","Subject name"," Course code","Data Structure","3DS201","Operating Systems","3OS201");
+	printf("\nEnter Choice for theory 2:\t");
+b:	scanf("%d",&opt);
+	if(opt==1)
+	{
+		NODE *b=t->t2s1;
+		if(b==NULL)
+			t->t2s1=a;
+		else
+		{
+			while(b->t2n)
+				b=b->t2n;
+			b->t2n=a;
+		}
+		ct2s1++;
+	}
+	else if(opt==2)
+	{
+		NODE *b=t->t2s2;
+		if(b==NULL)
+			t->t2s2=a;
+		else
+		{
+			while(b->t2n)
+				b=b->t2n;
+			b->t2n=a;
+		}
+		ct2s2++;
+	}
+	else
+	{
+		printf("\nEnter Option either 1 or 2:\t");
+		goto b;
+	}
+	printf("\n%-40s\n%-30s%-15s\n1.%-30s1) %-15s\n2.%-30s2) %-15s\n","Options for lab 1","Subject name"," Course code","Processor Architecture","3PA252","Data Base","2DB252");
+	printf("\nEnter Choice for Lab 1:  \t");
+c:	scanf("%d",&opt);
+	if(opt==1)
+	{
+		NODE *b=t->l1s1;
+		if(b==NULL)
+			t->l1s1=a;
+		else
+		{
+			while(b->l1n)
+				b=b->l1n;
+			b->l1n=a;
+		}
+		cl1s1++;
+	}
+	else if(opt==2)
+	{
+		NODE *b=t->l1s2;
+		if(b==NULL)
+			t->l1s2=a;
+		else
+		{
+			while(b->l1n)
+				b=b->l1n;
+			b->l1n=a;
+		}
+		cl1s2++;
+	}
+	else
+	{
+		printf("\nEnter Option either 1 or 2:\t");
+		goto c;
+	}
+	printf("\n%-40s\n%-30s%-15s\n1.%-30s1) %-15s\n2.%-30s2) %-15s\n","Options for lab 2","Subject name"," Course code","Computer Networks","3CN252","Progamming lab 1","3PL252");
+	printf("\nEnter Choice for Lab 2:  \t");
+d:	scanf("%d",&opt);
+	if(opt==1)
+	{
+		NODE *b=t->l2s1;
+		if(b==NULL)
+			t->l2s1=a;
+		else
+		{
+			while(b->l2n)
+				b=b->l2n;
+			b->l2n=a;
+		}
+		cl2s1++;
+	}
+	else if(opt==2)
+	{
+		NODE *b=t->l2s2;
+		if(b==NULL)
+			t->l2s2=a;
+		else
+		{
+			while(b->l2n)
+				b=b->l2n;
+			b->l2n=a;
+		}
+		cl2s2++;
+	}
+	else
+	{
+		printf("\nEnter Option either 1 or 2:\t");
+		goto d;
+	}
+	printf("\n%-40s\n%-30s%-15s\n1.%-30s1) %-15s\n2.%-30s2) %-15s\n3.%-30s3) %-15s","Options for electives","Subject name"," Course code","Image Processing","3IM201","Artificial intelligence","3AI201","Machine Learning","3ML201");
+	printf("\nEnter Choice for Elective: \t");
+e:	scanf("%d",&opt);
+	if(opt==1)
+	{
+		NODE *b=t->els1;
+		if(b==NULL)
+			t->els1=a;
+		else
+		{
+			while(b->eln)
+				b=b->eln;
+			b->eln=a;
+		}
+		cel1++;
+	}
+	else if(opt==2)
+	{
+		NODE *b=t->els2;
+		if(b==NULL)
+			t->els2=a;
+		else
+		{
+			while(b->eln)
+				b=b->eln;
+			b->eln=a;
+		}
+		cel2++;
+	}
+	else if(opt==3)
+	{
+		NODE *b=t->els3;
+		if(b==NULL)
+			t->els3=a;
+		else
+		{
+			while(b->eln)
+				b=b->eln;
+			b->eln=a;
+		}
+		cel3++;
+	}
+	else
+	{
+		printf("\nEnter Option either 1 or 2 or 3:\t");
+		goto e;
+	}
+	a->d=d;
+	a->m=m;
+	a->y=y;
+	a->next=a->t1n=a->t2n=a->eln=a->l1n=a->l2n=NULL;
+	if(t->st==NULL)
+		t->st=a;
+	else
+	{
+		NODE *b=t->st;
+		while(b->next)
+			b=b->next;
+		b->next=a;
+	}
+}
+
+void display(LIST *t)						
+{
+	NODE *d=t->st;
+	int i=0;
+	while(d)
+	{
+		if(i==0)
+		{
+			printf("\n\nDetails:\n\n");
+			printf(" %-15s |    %-15s|    %-15s","Roll No.","Name","Date");
+		}
+		printf("\n  %-15d|    %-15s|    %d/%d/%d",d->rno,d->nm,d->d,d->m,d->y);
+		d=d->next;
+		i=1;
+	}
+	if(!i)
+		printf("Empty list\n");
+}
+
+void displayt1(LIST *t,NODE *d)			//displaying theory 1 subject's student list
+{
+	int i=0;
+	while(d)
+	{
+		if(i==0)
+		{
+			printf("\n\nDetails:\n\n");
+			printf(" %-15s |    %-15s|    %-15s","Roll No.","Name","Date");
+		}
+		printf("\n  %-15d|    %-15s|    %d/%d/%d",d->rno,d->nm,d->d,d->m,d->y);		
+		d=d->t1n;
+		i=1;
+	}
+	if(!i)
+		printf("Empty list\n");
+}
+
+void displayt2(LIST *t,NODE *d)			//displaying theory 2 subject's student list
+{
+	int i=0;
+	while(d)
+	{
+		if(i==0)
+		{
+			printf("\n\nDetails:\n\n");
+			printf(" %-15s |    %-15s|    %-15s","Roll No.","Name","Date");
+		}
+		printf("\n  %-15d|    %-15s|    %d/%d/%d",d->rno,d->nm,d->d,d->m,d->y);
+		d=d->t2n;
+		i=1;
+	}
+	if(!i)
+		printf("Empty list\n");
+}
+
+void displayl1(LIST *t,NODE *d)			//displaying lab 1 subject's student list
+{
+	int i=0;
+	while(d)
+	{
+		if(i==0)
+		{
+			printf("\n\nDetails:\n\n");
+			printf(" %-15s |    %-15s|    %-15s","Roll No.","Name","Date");
+		}
+		printf("\n  %-15d|    %-15s|    %d/%d/%d",d->rno,d->nm,d->d,d->m,d->y);
+		d=d->l1n;
+		i=1;
+	}
+	if(!i)
+		printf("\nEmpty list\n");
+}
+
+void displayl2(LIST *t,NODE *d)			//displaying lab 2 subject's student list
+{
+	int i=0;
+	while(d)
+	{
+		if(i==0)
+		{
+			printf("\n\nDetails:\n\n");
+			printf(" %-15s |    %-15s|    %-15s","Roll No.","Name","Date");
+		}
+		printf("\n  %-15d|    %-15s|    %d/%d/%d",d->rno,d->nm,d->d,d->m,d->y);
+		d=d->l2n;
+		i=1;
+	}
+	if(!i)
+		printf("Empty list\n");
+}
+
+void displayel(LIST *t,NODE *d)			//displaying elective subject's student list
+{
+	int i=0;
+	while(d)
+	{
+		if(i==0)
+		{
+			printf("\n\nDetails:\n\n");
+			printf(" %-15s |    %-15s|    %-15s","Roll No.","Name","Date");
+		}
+		printf("\n  %-15d|    %-15s|    %d/%d/%d",d->rno,d->nm,d->d,d->m,d->y);
+		d=d->eln;
+		i=1;
+	}
+	if(!i)
+		printf("Empty list\n");
+}
+
+void change(LIST *t,int no)
+{
+	int opt;
+	NODE *f,*d,*c,*e,*b=t->els1;	
+	c=t->els1;  d=t->els2;  e=t->els3;
+	while(c!=NULL)
+	{
+		if(c->rno==no)	
+		{
+			printf("\n%-40s\n%-30s%-15s\n1.%-30s%-15s\n2.%-30s%-15s\n3.%-30s%-15s\n","Options for electives","Subject name","Course code","Image Processing","3IM201","Artificial intelligence","3AI201","Machine Learning","3ML201");
+			printf("\nEnter Choice for Elective: \t");
+			scanf("%d",&opt);
+			if(c==t->els1)
+				t->els1=c->eln;
+			else
+				b->eln=c->eln;
+			cel1--;
+			if(opt==1)
+			{
+				f=t->els1;
+				while(f!=NULL)
+				{
+					if(f->rno>no)
+						break;
+					d=f;
+					f=f->eln;
+				}
+				if(f==t->els1)
+					t->els1=c;
+				else
+					d->eln=c;
+				c->eln=f;
+				cel1++;
+			}
+		 	else if(opt==2)
+				{
+					f=t->els2;
+					while(f!=NULL)
+					{
+						if(f->rno>no)
+							break;
+						d=f;
+						f=f->eln;
+					}
+					if(f==t->els2)
+						t->els2=c;
+					else
+						d->eln=c;
+					c->eln=f;
+					cel2++;
+				}
+			else
+			{
+				f=t->els3;
+				while(f!=NULL)
+				{
+					if(f->rno>no)
+						break;
+					d=f;
+					f=f->eln;
+				}
+				if(f==t->els3)
+					t->els3=c;
+				else
+					d->eln=c;
+				c->eln=f;
+				cel3++;
+			}
+			return;				
+		}
+		b=c;
+		c=c->eln;
+	}
+	c=d;
+	b=c;
+	while(c!=NULL)
+	{
+		if(c->rno==no)	
+		{
+			printf("\n%-40s\n%-30s%-15s\n1.%-30s%-15s\n2.%-30s%-15s\n3.%-30s%-15s\n","Options for electives","Subject name"," Course code","Image Processing","3IM201","Artificial intelligence","3AI201","Machine Learning","3ML201");
+			printf("\nEnter Choice for Elective: \t");
+			scanf("%d",&opt);
+			if(c==t->els2)
+				t->els2=c->eln;
+			else
+				b->eln=c->eln;
+			cel2--;
+			if(opt==1)
+				{
+					f=t->els1;
+				while(f!=NULL)
+				{
+					if(f->rno>no)
+						break;
+					d=f;
+					f=f->eln;
+				}
+				if(f==t->els1)
+					t->els1=c;
+				else
+					d->eln=c;
+				c->eln=f;
+				cel1++;
+				
+				}
+		 	else if(opt==2)
+				{
+					f=t->els2;
+					while(f!=NULL)
+					{
+						if(f->rno>no)
+							break;
+						d=f;
+						f=f->eln;
+					}
+					cel2++;
+					if(f==t->els2)
+						t->els2=c;
+					else
+						d->eln=c;
+					c->eln=f;
+					
+				}
+			else
+			{
+				f=t->els3;
+				while(f!=NULL)
+				{
+					if(f->rno>no)
+						break;
+					d=f;
+					f=f->eln;
+				}
+				if(f==t->els3)
+					t->els3=c;
+				else
+					d->eln=c;
+				c->eln=f;
+				cel3++;
+				
+			}
+			return;			
+		}
+		b=c;
+		c=c->eln;
+	}
+	c=e;
+	b=c;
+	while(c!=NULL)
+	{
+		if(c->rno==no)	
+		{
+			printf("\n%-40s\n%-30s%-15s\n1.%-30s%-15s\n2.%-30s%-15s\n3.%-30s%-15s\n","Options for electives","Subject name"," Course code","Image Processing","3IM201","Artificial intelligence","3AI201","Machine Learning","3ML201");
+			printf("\nEnter Choice for Elective: \t");
+			scanf("%d",&opt);
+			if(c==t->els3)
+				t->els3=c->eln;
+			else
+				b->eln=c->eln;
+			cel3--;
+			if(opt==1)
+				{
+					f=t->els1;
+					while(f!=NULL)
+					{
+						if(f->rno>no)
+							break;
+						d=f;
+						f=f->eln;
+					}
+					if(f==t->els1)
+						t->els1=c;
+					else
+						d->eln=c;
+					c->eln=f;
+					cel1++;
+				
+				}
+		 	else if(opt==2)
+				{
+					f=t->els2;
+					while(f!=NULL)
+					{
+						if(f->rno>no)
+							break;
+						d=f;
+						f=f->eln;
+					}
+					if(f==t->els2)
+						t->els2=c;
+					else
+						d->eln=c;
+					c->eln=f;
+					cel2++;
+					
+				}
+			else
+			{
+				f=t->els3;
+				while(f!=NULL)
+				{
+					if(f->rno>no)
+						break;
+					d=f;
+					f=f->eln;
+				}
+				if(f==t->els3)
+					t->els3=c;
+				else
+					d->eln=c;
+				c->eln=f;
+				cel3++;
+				
+			}	
+			return;				
+		}
+		b=c;
+		c=c->eln;
+	}
+}
+
+void save(LIST *t)					 // Saving the data in list to the file stu.txt
+{
+	FILE *f=fopen("stu.txt","w");
+	fprintf(f,"%d %d %d %d %d %d %d %d %d %d %d\n",ct1s1,ct1s2,ct2s1,ct2s2,cl1s1,cl1s2,cl2s1,cl2s2,cel1,cel2,cel3);
+	NODE *a=t->st;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","Registered",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->next;
+	}
+	a=t->t1s1;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3SE201",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->t1n;
+	}
+	a=t->t1s2;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3DM201",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->t1n;
+	}
+	a=t->t2s1;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3DS201",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->t2n;
+	}
+	a=t->t2s2;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3OS201",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->t2n;
+	}
+	a=t->l1s1;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3PA252",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->l1n;
+	}
+	a=t->l1s2;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3DB252",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->l1n;
+	}
+	a=t->l2s1;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3CN252",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->l2n;
+	}
+	a=t->l2s2;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3PL252",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->l2n;
+	}
+	a=t->els1;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3IM201",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->eln;
+	}
+	a=t->els2;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3AI201",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->eln;
+	}
+	a=t->els3;
+	while(a)
+	{
+		fprintf(f,"%s %d %s %d %d %d\n","3ML201",a->rno,a->nm,a->d,a->m,a->y);
+		a=a->eln;
+	}
+	fclose(f);
+}
+
+int isvalid(int d,int m,int y)			//Checking the entered date is valid or not				
+{
+	if((m==2 && (y%400==0 || (y%100!=0 && y%4==0))) && d<30 && d>0)
+		return 1;
+	if((m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12) && d<32 && d>0)
+		return 1;
+	if((m==4 || m==6 || m==9 || m==11) && d<31 && d>0)
+		return 1;
+	if(m==2 && d<29 && d>0)
+		return 1;
+	return 0;
+}
+
+LIST p;								   //
